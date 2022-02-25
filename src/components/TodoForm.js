62cadfae69d4ff04605e957b4dcac742e8190d9f
Luis Andres Contreras <tremenditomen@gmail.com>
2022-02-25 12:53:03 -0700

    adding components to the todo list

---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+  const [input, setinput] = useState("");
+  const handleChange = (e) => {
+    setinput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setinput('')
+  };
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      />
+      <button className="todo-button">Add todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
